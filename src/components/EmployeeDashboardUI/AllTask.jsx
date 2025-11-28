a4c0947f7636e5c90112c9323584282f72a0d7ec
arman1104 <arman051215@gmail.com>
2025-11-28 20:54:57 +0530

    worked on  employee dashboard

---
+++ src/components/EmployeeDashboardUI/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div className="bg-[#1C1C1C] p-5 mt-6 h-48 overflow-auto rounded">
+      <div className="bg-red-400 py-2 px-4 mb-2 flex justify-between rounded">
+        <h2>Arman</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-400 py-2 px-4 mb-2 flex justify-between rounded">
+        <h2>Arman</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-blue-400 py-2 px-4 mb-2 flex justify-between rounded">
+        <h2>Arman</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-orange-400 py-2 px-4 mb-2 flex justify-between rounded">
+        <h2>Arman</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-indigo-400 py-2 px-4 mb-2 flex justify-between rounded">
+        <h2>Arman</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
