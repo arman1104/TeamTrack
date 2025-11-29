7ed48c59f39c3542cb9d01f9129222b1975f749b
arman1104 <arman051215@gmail.com>
2025-11-29 13:24:11 +0530

    worked on local storage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { setLocalStorage } from "./utils/LocalStorage";
+import { getLocalStorage } from "./utils/LocalStorage";
 
 const App = () => {
+  useEffect(() => {
+    setLocalStorage();
+    getLocalStorage();
+  });
   return (
     <>
       <Login />
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AuthContext from "./context/AuthContext.jsx";
-import TaskContext from "./context/TaskContext.jsx";
+// import TaskContext from "./context/TaskContext.jsx";
 
+localStorage.clear();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContext>
-      <TaskContext>
-        <App />
-      </TaskContext>
+      {/* <TaskContext> */}
+      <App />
+      {/* </TaskContext> */}
     </AuthContext>
   </StrictMode>
 );
