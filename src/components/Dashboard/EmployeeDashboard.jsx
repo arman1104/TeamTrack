c2324921a92909e582d68ab3eaa63dbf201fc63b
arman1104 <arman051215@gmail.com>
2025-12-04 13:47:59 +0530

    removed commented code

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import Header from "../EmployeeDashboardUI/Header";
-// import StatsSection from "../EmployeeDashboardUI/StatsSection";
-// import TaskList from "../TaskList/TaskList";
-
-// const EmployeeDashboard = ({ data }) => {
-//   console.log(data);
-//   return (
-//     <div className="p-10 sm:p-14 bg-[#1C1C1C] h-screen">
-//       <Header data={data} />
-//       <StatsSection data={data} />
-//       <TaskList data={data} />
-//     </div>
-//   );
-// };
-
-// export default EmployeeDashboard;
-
-// src/components/Dashboard/EmployeeDashboard.jsx
 import React from "react";
 import Header from "../EmployeeDashboardUI/Header";
 import StatsSection from "../EmployeeDashboardUI/StatsSection";
