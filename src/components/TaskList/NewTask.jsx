e99de06cf654d8ad6f5cfaf89abb600cc226f706
arman1104 <arman051215@gmail.com>
2025-12-11 21:38:19 +0530

    worked on some funtionality

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,30 +1,3 @@
-// import React from "react";
-
-// const NewTask = ({ data }) => {
-//   return (
-//     <div className="flex-shrink-0 h-full w-[350px] p-5 bg-indigo-400 rounded-lg">
-//       <div className="flex justify-between items-center">
-//         <h3 className="bg-orange-700 text-sm px-3 py-1 rounded">
-//           {data.category}
-//         </h3>
-//         <h4 className="text-base">{data.date}</h4>
-//       </div>
-
-//       <h2 className="mt-5 text-xl font-semibold">{data.title}</h2>
-//       <p className="text-sm mt-2">{data.description}</p>
-
-//       <div className="mt-4">
-//         <button className="bg-blue-500 rounded font-medium py-1 px-2 text-xs">
-//           Accept Task
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NewTask;
-
-// src/components/TaskList/NewTask.jsx
 import React from "react";
 
 const NewTask = ({ data, onAccept }) => {
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import AcceptTask from "./AcceptTask";
-// import NewTask from "./NewTask";
-// import CompleteTask from "./CompleteTask";
-// import FailedTask from "./FailedTask";
-
-// const TaskList = ({ data }) => {
-//   if (!data || !data.tasks) return null;
-
-//   return (
-//     <div
-//       id="tasklist"
-//       className="flex items-center justify-start gap-4 flex-nowrap overflow-x-auto h-[50%] w-full py-5 mt-10 rounded-lg"
-//     >
-//       {data.tasks.map((task, idx) => {
-//         if (task.newTask) return <NewTask key={idx} data={task} />;
-//         if (task.active) return <AcceptTask key={idx} data={task} />;
-//         if (task.completed) return <CompleteTask key={idx} data={task} />;
-//         if (task.failed) return <FailedTask key={idx} data={task} />;
-//         return null;
-//       })}
-//     </div>
-//   );
-// };
-
-// export default TaskList;
-
-// src/components/TaskList/TaskList.jsx
 import React, { useContext } from "react";
 import AcceptTask from "./AcceptTask";
 import NewTask from "./NewTask";
